b70ba4e247c5974519fe5e93fbaf5f87c071c87b
toty1992 <gesman@andreani.com>
2020-09-27 16:13:32 -0300

    rutas y componentes + redux

--- src/footer.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-
-const Footer = () =>
-            <footer>
-              <p>&copy; copyrights</p>
-            </footer>
-
-export default Footer
--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import ReactDOM from "react-dom"
 import React from "react"
-import App from "./app"
-
+import App from "./UI/layout/app"
+import {Provider} from "react-redux"
+import store from './API/store'
+import { BrowserRouter } from "react-router-dom"
 
 
 ReactDOM.render(
-    <App/>,
+    <Provider store={store}>
+        <BrowserRouter>
+            <App/>
+        </BrowserRouter>
+    </Provider>,
     document.getElementById("root")
 )
